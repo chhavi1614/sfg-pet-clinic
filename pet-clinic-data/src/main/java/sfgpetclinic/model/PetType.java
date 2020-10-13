21fbe02d9d70c20b04bfb8385b8ca1814ae9e8ef
chhavi1614 <chhavi.jain.mat17@itbhu.ac.in>
2020-10-13 21:15:42 +0530

    Adding Project Lombok.

--- pet-clinic-data/src/main/java/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/PetType.java
@@ -1,9 +1,15 @@
 package sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="types")
 public class PetType extends BaseEntity{
@@ -11,11 +17,8 @@ public class PetType extends BaseEntity{
     @Column(name="name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public PetType(Long id, String name) {
+        super(id);
         this.name = name;
     }
 }
